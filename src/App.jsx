d9f4025cb5194f3a628dd45f92584c7a59aa95a2
Tavonga Gillian <tzvigumbu@gmail.com>
2024-06-19 17:49:19 +1200

    Updated my page

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,59 @@ import { JobPages } from "./pages/JobPages";
 import { NotFoundPage } from "./pages/NotFoundPage";
 import { JobPage, jobLoader } from "./pages/JobPage";
 import { AddJobPage } from "./pages/AddJobPage";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path='/jobs' element={<JobPages />} />
-      <Route path='/add-job' element={<AddJobPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
-      <Route path='*' element={<NotFoundPage />} />
-    </Route>
-  )
-);
+import { EditJobPAge } from "./pages/EditJobPAge";
 
 const App = () => {
+  const addJob = async (newJob) => {
+    await fetch("/api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  const deleteJob = async (id) => {
+    await fetch(`/api/jobs/${id}`, {
+      method: "DELETE",
+      headers: {},
+      body: JSON.stringify(id),
+    });
+    return;
+  };
+  const updateJob = async (jobs) => {
+    await fetch(`/api/jobs/${jobs.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jobs),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobPages />} />
+        <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+        <Route
+          path='/jobs/:id'
+          element={<JobPage deleteJob={deleteJob} />}
+          loader={jobLoader}
+        />
+        <Route
+          path='/edit-jobs/:id'
+          element={<EditJobPAge updateJobSubmit={updateJob} />}
+          loader={jobLoader}
+        />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
 };
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const Card = ({ children, bg = "bg-gray-100" }) => {
-  return <div className={`${bg} rounded-lg shadow-md`}>{children}</div>;
+  return <div className={`${bg} rounded-lg p-5 shadow-md`}>{children}</div>;
 };
 
 // export default Card
